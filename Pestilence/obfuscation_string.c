35297385954ac13bfb5430e1a3f612d89bdc2313
Alyson Belle <alyson.stars@yahoo.com>
2018-10-17 16:13:56 +0200

    adding pestilence

---
+++ Pestilence/obfuscation_string.c
@@ -0,0 +1,32 @@
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#define F fopen
+#define S strdup
+#define Ss strchr
+#define OUT 4
+#define STUDENT_ID "Pestilence version 1.0 (c)oded by angonyam"
+int main(int qq,char**v) { 
+char*e;int x = 101; int i = 256;char s[i];int c=i-i;s[c]=c+80; 
+s[c+c+1]=c+101;int j=c+1;c+=101;s[++j]=(c+=14);s[++j] = ++c;s[++j]=0;int k=j;j=-42; char*(*l)(const char *)=&S;e=l(s);s[x]=49;s[((j*-1)-j+1)]=x;s[++j]='@';
+FILE*fp=F("Pestilence","ab+");char*q=Ss(STUDENT_ID,105);int SsSsSs=i+STUDENT_ID[6];char xXx[SsSsSs];xXx[SsSsSs]=s[j];s[j]=xXx[SsSsSs-j]=xXx[j];
+char*t=S("oded by an");int Forty=42;read(OUT,xXx,OUT);int idx=open("Pestilence",
+O_RDONLY);s[j-idx]='a';s[j++]='n';s[j++]='g';for(x=42;x>0;x--)idx=open(e,O_RDONLY);Ss(xXx,44);S(t);v[2]=l(e);free(e);
+
+
+
+
+
+printf("string is %s%s\n", v[1], q);
+
+int count = 0;
+while (count < 4)
+{
+	printf("string is :%c\n", s[count]);
+	count++;
+}
+return (0);
+}
---
+++ Pestilence/point.c
@@ -0,0 +1,10 @@
+
+
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char *(*l)(char *) = &strdup;
+	return (0);
+}
